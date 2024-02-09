2e4b5f77761ae921003b4ee8e2ddc7dfa404cd5f
CoderKit <wongyuekit@hotmail.com>
2024-02-09 12:18:58 +0800

    removed smtp credentials

--- src/main/java/com/testApp/demo/Service/Service.java
+++ src/main/java/com/testApp/demo/Service/Service.java
@@ -2,7 +2,6 @@ package com.testApp.demo.Service;
 
 import com.testApp.demo.Model.CourseDTO;
 import com.testApp.demo.Model.CourseModel;
-import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,31 +12,31 @@ import java.io.IOException;
 
 @org.springframework.stereotype.Service
 public class Service {
+
+    @Autowired
+    private CourseDTO courseDTO;
     @Autowired
     private JavaMailSender mailSender;
-
-    @Autowired(required = false)
-    private CourseDTO courseDTO;
     String line = "";
 
     public void saveCourseData(){
         try {
             BufferedReader br = new BufferedReader(new FileReader("src/main/resources/data.csv"));
             br.readLine();//to ignore 1st line
-////            while((line = br.readLine()) != null) {
-////                String[] data = line.split(",");
-////                CourseModel courseModel = new CourseModel();
-////
-////                courseModel.setEmployeeCode(Long.valueOf(data[0]));
-////                courseModel.setName(data[1]);
-////                courseModel.setEmail(data[2]);
-////                courseModel.setPhoneNum(Integer.parseInt(data[3]));
-////                courseModel.setCourseCode(data[4]);
-////                courseModel.setGrade(data[5]);
-////                courseModel.setLectureName(data[6]);
-////                courseDTO.save(courseModel);
-//
-//            }
+            while((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                CourseModel courseModel = new CourseModel();
+
+                courseModel.setEmployeeCode(Long.valueOf(data[0]));
+                courseModel.setName(data[1]);
+                courseModel.setEmail(data[2]);
+                courseModel.setPhoneNum(Integer.parseInt(data[3]));
+                courseModel.setCourseCode(data[4]);
+                courseModel.setGrade(data[5]);
+                courseModel.setLectureName(data[6]);
+                courseDTO.save(courseModel);
+
+            }
             sendEmail();
 
         } catch (IOException e) {
